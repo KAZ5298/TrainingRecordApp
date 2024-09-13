93721cd2c55974a72b5fc01c440d974919aeca7c
KAZUHIKO <kazuhiko.d@gmail.com>
2024-09-13 14:27:33 +0900

    微修正

--- src/main/java/com/example/training_app/model/User.java
+++ src/main/java/com/example/training_app/model/User.java
@@ -15,40 +15,40 @@ import lombok.Data;
 @Entity
 @Table(name = "users")
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column(nullable = false, unique = true)
-    private String name;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String familyName;
-
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private int age;
-    
-    @Column(nullable = false)
-    private double weight;
-    
-    @Column(nullable = false)
-    private double height;
-    
-    @Column(nullable = false)
-    private int gender;
-    
-    private String profileImage;
-
-    @Column(nullable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String familyName;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private int age;
+	
+	@Column(nullable = false)
+	private double weight;
+	
+	@Column(nullable = false)
+	private double height;
+	
+	@Column(nullable = false)
+	private int gender;
+	
+	private String profileImage;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
 }
